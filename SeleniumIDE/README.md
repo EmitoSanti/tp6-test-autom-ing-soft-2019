94a859396842b51c856bb7e607be4bfe7d9b2d55
Emiliano Santi <emilianosanti@hotmail.com>
2019-11-03 23:59:38 -0300

    Primer Commit TP6 ING de Soft 2019

---
+++ SeleniumIDE/README.md
@@ -0,0 +1,6 @@
+# Trabajo Práctico N° 6 - Testing Automatizado
+## Ingeniería de Software 2019
+Estudiantes: Emiliano Santi - Enzo Fernandez - Pablo Arias
+
+## PUNTO 1 y 2
+En este directorio estan los archivos de los proyectos de SeleniumIDE, del punto 1 y 2
---
+++ src/PrimerTestAutomatizado/PrimerTestAutomatizado.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PrimerTestAutomatizado;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+/**
+ *
+ * @author emili
+ */
+public class PrimerTestAutomatizado {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static WebDriver driver = null;
+    public static void main(String[] args) {
+        // Prueba ChromeDriver
+        //System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe --whitelist-ip %");
+        //driver = new ChromeDriver();
+        //driver.get("http://newtours.demoaut.com/");
+    }
+    
+}
